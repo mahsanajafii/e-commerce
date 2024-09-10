83f49eb87a7094fe18ce2138fc284c2dbf0eda4c
Marziye <pakdel.marzieh1380@gmail.com>
2024-09-10 21:11:23 +0330

    style: update base font size from 16px to 10px

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        base: '10px', // root font size
+      },
+    },
   },
   plugins: [],
 };

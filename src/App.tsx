b8dc40daf0208c6931fd77b990432a5e687b7843
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-09-10 16:13:25 +0330

    feat: :sparkles: create Rputer.tsx
    #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { RouterProvider } from "react-router-dom";
+import Router from "./routes/Router";
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={Router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/routes/Router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "*",
+    element: <div>arash</div>,
+  },
+  {
+    path: "/login",
+    element: <div>arash</div>,
+  },
+  {
+    path: "/register",
+    element: <div>arash</div>,
+  },
+]);
+
+export default routes;

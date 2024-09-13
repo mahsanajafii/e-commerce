97f42e73a64472ac738138ffaeb513f2da069b7b
Sia <apf.sorrow@gmail.com>
2024-09-13 23:47:57 +0430

    remove children from badge component

--- src/components/common/badge/Badge.tsx
+++ src/components/common/badge/Badge.tsx
@@ -1,11 +1,11 @@
-interface IBadgeProps extends React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode;
+interface IBadgeProps {
+  badgeTitle: string;
 }
 
 const Badge: React.FC<IBadgeProps> = ({
-  children
+  badgeTitle
 }) => { return (
-  <div className='w-[8.2rem] h-[2.4rem] rounded-full bg-[#831747] flex justify-center items-center px-4 py-1'>{children}</div>
+  <div className='w-[8.2rem] h-[2.4rem] rounded-full bg-[#831747] flex justify-center items-center px-4 py-1'>{badgeTitle}</div>
 );
 }
 

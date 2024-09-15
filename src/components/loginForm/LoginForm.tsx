7f86f321300718bcb75ce3cac327a70c615f71b8
mahsanajafi <mahsa.najafi2006@gmail.com>
2024-09-15 10:07:12 +0330

    Resolved merge conflicts

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,29 +1,39 @@
 import { Link } from "react-router-dom";
 import Input from "../common/input/Input";
 import Button from "../common/button/Button";
-interface Props {
-  handleSubmitForm: (e: React.FormEvent<HTMLFormElement>) => void;
-}
+import { useForm, FieldValues } from "react-hook-form";
 
-const LoginForm = ({ handleSubmitForm }: Props) => {
+const LoginForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex flex-col gap-8 w-[53.1rem] h-[35.6rem] font-Iran-Yekan">
       <h1 className="text-text-primary text-[2rem] font-bold">ورود</h1>
-      <form onSubmit={handleSubmitForm}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Input
-          inputStyle="bg-white w-full h-[4.2rem] px-[0.9rem] py-[1rem] rounded-xl outline-none text-[1.6rem]"
-          id="login-emailInp"
-          labelStyle="block text-[1.6rem] pb-3 pt-3"
+          {...register("email")}
+          id="email"
+          type="email"
           placeholder="ایمیل خود را وارد کنید"
           label="ایمیل"
-        />
-        <Input
           inputStyle="bg-white w-full h-[4.2rem] px-[0.9rem] py-[1rem] rounded-xl outline-none text-[1.6rem]"
-          id="login-passwordInp"
           labelStyle="block text-[1.6rem] pb-3 pt-3"
+        />
+
+        <Input
+          {...register("password")}
+          id="password"
+          type="password"
           placeholder="رمز عبور خود را وارد کنید"
           label="رمز عبور"
+          inputStyle="bg-white w-full h-[4.2rem] px-[0.9rem] py-[1rem] rounded-xl outline-none text-[1.6rem]"
+          labelStyle="block text-[1.6rem] pb-3 pt-3"
         />
+
         <Button className="flex justify-center items-center bg-primary-main w-[7.4rem] h-[4.8rem] text-white rounded-lg mt-8 text-xl px-[0.8rem] py-[2.4rem]">
           ورود
         </Button>
--- src/layout/content/Content.tsx
+++ src/layout/content/Content.tsx
@@ -6,7 +6,7 @@ interface IContent {
 
 const content = ({children} : IContent) => {
   return (
-    <section className='w-full'>{children}</section>
+    <section className='w-full bg-base-backgrond'>{children}</section>
   )
 }
 

0e69ea387ec71d2f9d47aca3e3786fb061493d63
mahsanajafi <mahsa.najafi2006@gmail.com>
2024-09-15 13:53:49 +0330

    add hook form

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,84 +1,90 @@
-import React ,{ useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 import Input from "../common/input/Input";
 import Button from "../common/button/Button";
 
-const Profile :React.FC= () => {
-  const [fullname, setFullName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [repassword, setRepassword] = useState<string>("");
+const Profile: React.FC = () => {
+  interface IFormInput {
+    fullname: string;
+    email: string;
+    password: string;
+    repassword: string;
+  }
+  const { register, handleSubmit, control } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
   const inputStyle =
-    "flex flex-col w-full h-11 bg-base-text-field text-[1.6rem]  text-text-secondary border  border-base-text-field-stroke rounded-lg pt-[.63rem] pb-[.7rem] px-[.56rem]";
-  const labelStyle = `w-[40rem] h-6 text-text-primary text-[1.6rem]`;
+    "flex flex-col w-full   bg-base-text-field text-[1.6rem]  text-text-secondary border  border-base-text-field-stroke rounded-lg pt-[1rem] pb-[1.1rem] px-[.9rem]";
+  const labelStyle = `w-full h-[2.4rem]  text-text-primary text-[1.6rem]`;
 
   return (
-    <div className="w-full h-full  justify-center items-center">
-      <form className="w-[44rem] h-[27.8rem] mt-44 gap-4 ml-[40rem] flex flex-col items-start   justify-center">
-        <h1 className=" font-Iran-Yekan  font-medium text-[2.4rem]">
+    <div className=" pt-[1.8rem] flex justify-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-[64rem] gap-[1.6rem] h-[44rem] flex flex-col"
+      >
+        <h1 className=" font-Iran-Yekan font-medium text-[2.4rem]">
           بروزرسانی پروفایل
         </h1>
-        <div className="w-full  h-[4.6rem] gap-2">
+        <div className="w-full h-[7.4rem] gap-2">
           <Input
             inputStyle={inputStyle}
             labelStyle={labelStyle}
-            type="text"
+            {...register("fullname")}
             placeholder="نام خود را وارد نمایید"
-            value={fullname}
             label="نام"
-            id="1"
-            onChange={(e) => setFullName(e.target.value)}
+            id="fullname"
           />
         </div>
-        <div className="w-full  h-[4.6rem] gap-2">
+        <div className="w-full h-[7.4rem] gap-2">
           <Input
             inputStyle={inputStyle}
             labelStyle={labelStyle}
             type="email"
+            {...register("email")}
             placeholder="ایمیل خود را وارد نمایید"
-            value={email}
             label="ایمیل"
-            id="2"
-            onChange={(e) => setEmail(e.target.value)}
+            id="email"
           />
         </div>
 
-        <div className="w-full  h-[4.6rem] gap-2">
+        <div className="w-full  h-[7.4rem] gap-2">
           <Input
             inputStyle={inputStyle}
             labelStyle={labelStyle}
             type="password"
+            {...register("password")}
             placeholder="رمز عبور خود را وارد نمایید"
-            value={password}
             label="رمز عبور"
-            id="3"
-            onChange={(e) => setPassword(e.target.value)}
+            id="password"
           />
         </div>
 
-        <div className="w-full  h-[4.6rem] gap-2">
+        <div className="w-full  h-[7.4rem] gap-2">
           <Input
             inputStyle={inputStyle}
             labelStyle={labelStyle}
             type="password"
             placeholder="تکرار رمز عبور خود را وارد نمایید"
-            value={repassword}
-            id="4"
+            {...register("repassword")}
+            id="password"
             label="تکرار رمز عبور"
-            onChange={(e) => setRepassword(e.target.value)}
           />
         </div>
 
-        <div className="w-full h-9 justify-between items-center flex flex-row">
+        <div className="w-full h-[3.6rem] justify-between items-center flex flex-row">
           <Button
             children="سفارشات من"
-            className="w-[6.4rem] h-9 rounded-lg  p-2 text-center bg-primary-main text-text-button text-sm"
+            className="w-[10rem] h-full rounded-lg  p-2 text-center bg-primary-main text-text-button text-[1.4rem]"
           />
           <Button
+            onClick={handleSubmit(onSubmit)}
             children=" بروزرسانی"
-            className="w-[6.4rem] h-9 rounded-lg p-2 text-center bg-primary-main text-text-button text-sm"
+            className="w-[8.1rem] h-full rounded-lg p-2 text-center bg-primary-main text-text-button text-[1.4rem]"
           />
         </div>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };

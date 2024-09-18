89d3dae29f13978de5611349c4ec3c6d3688e532
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-09-18 11:31:56 +0330

    feat: :sparkles: create cart component
    create CardInfo in cart folder for use in the Cart Component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,20 +1,17 @@
-import { MdDeleteForever } from "react-icons/md";
+import CardInfo from "./cardInfo/CardInfo";
 import Button from "../common/button/Button";
+
 const Cart = () => {
   return (
     <div className="h-full flex justify-center items-center">
-      <div className="bg-red-500 w-[90%] h-[48.4rem] gap-[3.2rem] flex flex-col">
-        <div className="bg-yellow-400 w-full h-[70%]">
-          <div className="bg-slate-500 flex justify-between items-center h-[25%]">
-            <div></div>
-            <div></div>
-            <div className="bg-red-500 flex gap-[1.6rem] pl-3 justify-center items-center">
-              <select className="w-[5.2rem] h-[2.3rem]"></select>
-              <MdDeleteForever className="w-[1.4rem] h-[1.6rem] text-red-700 cursor-pointer" />
-            </div>
-          </div>
+      <div className="w-[90%] h-[48.4rem] gap-[3.2rem] flex flex-col">
+        <div className="p-6  w-full flex flex-col gap-5 h-[60%] overflow-auto">
+          <CardInfo />
+          <CardInfo />
+          <CardInfo />
+          <CardInfo />
         </div>
-        <div className="w-[50%] h-[30%] flex flex-col justify-between">
+        <div className=" w-[50%] h-[40%] flex flex-col justify-between">
           <p className="text-[2rem] font-medium font-Iran-Yekan text-text-primary">
             تعداد (3)
           </p>

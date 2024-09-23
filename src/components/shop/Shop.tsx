b17f85d3c9b235ec2e175312a4b26102a496d62c
mahsanajafi <mahsa.najafi2006@gmail.com>
2024-09-24 02:43:56 +0330

    edit shop page and shop component

--- src/components/shop/Shop.tsx
+++ src/components/shop/Shop.tsx
@@ -1,121 +1,62 @@
 import Button from "../common/button/Button";
-import Checkbox from "../common/checkbox/Checkbox";
 import FilterByGroup from "../common/filterByGroup/FilterByGroup";
-import ShopProductCard from "../common/productCard/shopProductCard/ShopProductCard"
+import Input from "../common/input/Input";
+import ShopProductCard from "../common/productCard/shopProductCard/ShopProductCard";
 interface IProductCardProps {
-    productImg: string;
-    productTitle: string;
-    badgeTitle: string;
-    brandTitele: string;
-    description: string;
-    
-  }
-  interface IShopProps {
-    shopProducts: IProductCardProps[];
-} 
-const Shop:React.FC<IShopProps> = () => {
-    const shopCarts=[
-         {
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           },
-           {
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           },
-           {
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           },
-           {
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           },{
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           },
-           {
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           },
-           {
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           },{
-             productImg: "جینگیلی مستون",
-             productTitle: "Apple iPhone 14 Pro",
-             badgeTitle: "10,000 تومان",
-             brandTitele: "َApple",
-             description: " گیران است گیران تر از هر گیران گوشی اپل ماست این گیران است این گیران است ما پول نداریم",
-           }
-     ]
-     const brands=["Apple","Microsoft"]
+  productImg: string;
+  productTitle: string;
+  badgeTitle: string;
+  brandTitele: string;
+  description: string;
+}
+interface IShopProps {
+  shopProducts: IProductCardProps[];
+}
+const Shop: React.FC<IShopProps> = ({ shopProducts }) => {
+  const brands = ["Apple", "Microsoft"];
   return (
     <main className="w-full h-full justify-center  flex ">
-    <section className="w-[122rem] h-[60rem]  flex flex-row justify-start items-center mt-14 gap-24">
-        {/* right....................................................... */}
-        {/* {
-  className?: string;
-  labelStyle?: string;
-  label?: string;
-  id?: string;
-  checked?: boolean;
-  onChange?: (checked: boolean) => void;
-} */}
-    <section className="flex flex-col justify-start items-center h-full bg-base-side w-1/4">
-    <div className=" bg-yellow-200 w-[95%] h-[32.8rem] gap-16 pt-5 flex flex-col justify-center items-center">
-        <div className="w-full bg-pink-200 gap-2 flex flex-col justify-center items-center">
-            <Button className="bg-base-menu w-full rounded-2xl py-3 px-[6.5rem] ">فیلتر برند</Button>
-            {
-                brands.map((brand,index)=>(
+      <section className="w-[122rem] h-[60rem]  flex flex-row justify-start items-center mt-14 gap-24">
+        <section className="flex flex-col justify-start items-center h-full bg-base-side w-[27rem]">
+          <div className="w-[25rem]  text-[1.6rem] leading-10  font-normal gap-16 flex flex-col pt-5 justify-center items-center">
+            <FilterByGroup topics={brands} />
+            <div className="flex flex-col items-center justify-center gap-3">
+              <div className="bg-white  text-text-primary  text-[1.6rem] leading-10  font-normal w-[24rem] h-[4rem] rounded-full flex justify-center items-center  py-1 mb-8">
+                فیلتر قیمت
+              </div>
+              <Input
+                inputStyle={
+                  "w-80 h-16 text-[1.4rem] leading-8 font-normal text-text-secondary rounded-xl border-gray-200 px-5 py-4 border"
+                }
+                labelStyle={""}
+                placeholder="قیمت را وارد نمایید"
+                id={"قیمت"}
+              />
+            </div>
+            <Button
+              className="w-80 h-10 text-[1.6rem] leading-10  font-normal border border-gray-400 rounded-md  text-center"
+              children={"حذف فیلتر ها"}
+            />
+          </div>
+        </section>
 
-                    <Checkbox key={index} className="rounded-full flex flex-row justify-start bg-blue-400" label={brand}/>
-                ))
-            }
-            <FilterByGroup/>
-        </div>
-        
-        
-        <div className="w-full bg-pink-200  flex flex-col justify-center items-center">palo</div>
-    </div>
-    
-    
-    
-    </section>
-
-    {/* left....................................................... */}
-   
-   
-    <section className=" grid grid-cols-3 gap-4 h-full w-full overflow-y-scroll overflow-x-hidden">
-        {shopCarts.map((cart,index)=>(
-
-    <ShopProductCard  key={index} flexOptional="flex-col" heightOptional="h-[34.7rem]" productTitle={cart.productTitle}  productImg={cart.productImg}  badgeTitle={cart.badgeTitle}  brandTitele={cart.brandTitele}  description={cart.description}/>
-        ))}
-    </section>
-    </section>
+        <section className=" grid grid-cols-3 gap-4 h-full w-full overflow-y-scroll overflow-x-hidden">
+          {shopProducts.map((product, index) => (
+            <ShopProductCard
+              key={index}
+              flexOptional="flex-col"
+              heightOptional="h-[34.7rem]"
+              productTitle={product.productTitle}
+              productImg={product.productImg}
+              badgeTitle={product.badgeTitle}
+              brandTitele={product.brandTitele}
+              description={product.description}
+            />
+          ))}
+        </section>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
--- src/pages/shopPage/ShopPage.tsx
+++ src/pages/shopPage/ShopPage.tsx
@@ -2,8 +2,53 @@ import Shop from "../../components/shop/Shop"
 
 
 const ShopPage = () => {
+    const shopCarts=[
+        {
+            productImg: " تنلتناتن",
+            productTitle: "Apple iPhone 14 Pro",
+            badgeTitle: "10,000 تومان",
+            brandTitele: "َApple",
+            description:"فلز و شیشه (قاب جلو و پشت شیشه‌ای با فریمی از جنس آلومینیوم) دارای استاندارد IP68 برای مقاومت در برابر آب (به مدت 30 دقیقه در عمق 6 متر)",
+          },
+        {
+            productImg: " تنلتناتن",
+            productTitle: "Apple iPhone 14 Pro",
+            badgeTitle: "10,000 تومان",
+            brandTitele: "َApple",
+            description:"فلز و شیشه (قاب جلو و پشت شیشه‌ای با فریمی از جنس آلومینیوم) دارای استاندارد IP68 برای مقاومت در برابر آب (به مدت 30 دقیقه در عمق 6 متر)",
+          },
+        {
+            productImg: " تنلتناتن",
+            productTitle: "Apple iPhone 14 Pro",
+            badgeTitle: "10,000 تومان",
+            brandTitele: "َApple",
+            description:"فلز و شیشه (قاب جلو و پشت شیشه‌ای با فریمی از جنس آلومینیوم) دارای استاندارد IP68 برای مقاومت در برابر آب (به مدت 30 دقیقه در عمق 6 متر)",
+          },
+        {
+            productImg: " تنلتناتن",
+            productTitle: "Apple iPhone 14 Pro",
+            badgeTitle: "10,000 تومان",
+            brandTitele: "َApple",
+            description:"فلز و شیشه (قاب جلو و پشت شیشه‌ای با فریمی از جنس آلومینیوم) دارای استاندارد IP68 برای مقاومت در برابر آب (به مدت 30 دقیقه در عمق 6 متر)",
+          },
+        {
+            productImg: " تنلتناتن",
+            productTitle: "Apple iPhone 14 Pro",
+            badgeTitle: "10,000 تومان",
+            brandTitele: "َApple",
+            description:"فلز و شیشه (قاب جلو و پشت شیشه‌ای با فریمی از جنس آلومینیوم) دارای استاندارد IP68 برای مقاومت در برابر آب (به مدت 30 دقیقه در عمق 6 متر)",
+          },
+        {
+            productImg: " تنلتناتن",
+            productTitle: "Apple iPhone 14 Pro",
+            badgeTitle: "10,000 تومان",
+            brandTitele: "َApple",
+            description:"فلز و شیشه (قاب جلو و پشت شیشه‌ای با فریمی از جنس آلومینیوم) دارای استاندارد IP68 برای مقاومت در برابر آب (به مدت 30 دقیقه در عمق 6 متر)",
+          },
+         
+    ]
   return (
-<Shop/>
+<Shop shopProducts={shopCarts}/>
   )
 }
 

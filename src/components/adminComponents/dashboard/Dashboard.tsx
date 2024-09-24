477d3eabb5bcba26800fc10e0d1e5ebf6feb46fa
Marziye <pakdel.marzieh1380@gmail.com>
2024-09-24 15:09:10 +0330

    feat: :lipstick: add some missing styles
    #78

--- src/components/adminComponents/dashboard/Dashboard.tsx
+++ src/components/adminComponents/dashboard/Dashboard.tsx
@@ -25,8 +25,8 @@ const Dashboard = () => {
             <div className="flex gap-20 w-[80%]">
                 {items.map((item, index) => {
                     return(
-                        <div key={index} className="flex flex-col grow gap-8 px-8 py-[1.6rem] bg-base-menu">
-                            <span className="bg-primary-main text-text-button w-[4.8rem] h-[4.8rem] rounded-full flex justify-center items-center">$</span>
+                        <div key={index} className="flex flex-col grow gap-8 rounded-[0.8rem] px-8 py-[1.6rem] bg-base-menu">
+                            <span className="bg-primary-main text-[1.6rem] text-text-button w-[4.8rem] h-[4.8rem] rounded-full flex justify-center items-center">$</span>
                             <div className="">
                                 <h4 className="text-text-secondary text-[1.6rem] font-normal">{item.title}</h4>
                                 <p className="text-text-primary text-[2rem] font-bold">{item.value}</p>
@@ -34,7 +34,7 @@ const Dashboard = () => {
                         </div>
                     )
                 })}
-                
+
             </div>
             <div></div>
         </div>

b6f2e0418df7334e05e79aeafc8e4a77ffc7ebb4
Marziye <pakdel.marzieh1380@gmail.com>
2024-10-01 15:44:32 +0330

    fix: :bug: api call
    #102

--- src/components/generalComponents/orders/Orders.tsx
+++ src/components/generalComponents/orders/Orders.tsx
@@ -1,20 +1,67 @@
+import { useEffect, useState } from "react";
 import Table from "../../common/table/Table";
+// import axios from "axios";
+import axiosClient from "../../../api/axiosClient";
 
-interface IOrder {
-  [index: string]: string | number,
-}
-
-interface IOrdersProps {
-    items : IOrder[];
+interface IOrders {
+  "عکس": string,
+  "نام محصول": string,
+  "تاریخ" : string,
+  "قیمت نهایی": string,
+  "پرداخت" : string,
+  "ارسال" : string,
+  "عملیات" : string,
 }
 
 const THs = ["عکس" , "نام محصول", "تاریخ", "قیمت نهایی", "پرداخت", "ارسال", "عملیات"];
 
+// const items = [
+//   {
+//     "عکس": "zsadfsdf",
+//     "نام محصول": "iphone promax 15",
+//     "تاریخ" : "1403/06/25",
+//     "قیمت نهایی": "100000$",
+//     "پرداخت" : "پرداخت شده",
+//     "ارسال" : "در حال ارسال",
+//     "عملیات" : "جزئیات",
+//   },
+//   {
+//     "عکس": "zsadfsdf",
+//     "نام محصول": "iphone promax 15",
+//     "تاریخ" : "1403/06/25",
+//     "قیمت نهایی": "100000$",
+//     "پرداخت" : "پرداخت نشده",
+//     "ارسال" : "ارسال شده",
+//     "عملیات" : "جزئیات",
+//   },
+//   {
+//     "عکس": "zsadfsdf",
+//     "نام محصول": "iphone promax 15",
+//     "تاریخ" : "1403/06/25",
+//     "قیمت نهایی": "100000$",
+//     "پرداخت" : "پرداخت نشده",
+//     "ارسال" : "ارسال نشده",
+//     "عملیات" : "جزئیات",
+//   },
+// ];
 
-const Orders : React.FC<IOrdersProps> = ({items}) => {
+const Orders : React.FC = () => {
+  const [orders, setOrders] = useState<IOrders[]>([])
+  
+  useEffect(() => {
+    fetchOrders()
+  },[])
+
+  async function fetchOrders() {
+    const allOrders = await axiosClient.get("/orders/mine").then((res) => {
+      setOrders(res.data)
+    })
+    console.log(allOrders)
+  }
+
   return (
     <div className="min-h-screen pt-24 px-8">
-      <Table optionalWidth="w-full" items={items} headers={THs} />
+      <Table optionalWidth="w-full" items={orders} headers={THs} />
     </div>
   )
 }

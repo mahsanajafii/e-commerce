ee622ed2e4e375ec1add9e1bdec9bf68afa701a9
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-10-03 18:07:27 +0330

    delete timeout from useLgin and useRegister and add useLogout into the interceptors

--- src/components/generalComponents/checkout/Checkout.tsx
+++ src/components/generalComponents/checkout/Checkout.tsx
@@ -1,13 +1,8 @@
 import Table from "../../common/table/Table";
 import Status from "../../common/status/Status";
-import { useEffect } from "react";
-import authService from "../../../services/authService";
 const headers = ["عکس", "نام محصول", "تعداد", "قیمت", "قیمت نهایی"];
 
 const Checkout = () => {
-  useEffect(() => {
-    authService.getAllUsers();
-  }, []);
   return (
     <div className="flex justify-center items-start gap-[5.6rem] w-full h-full p-[9.7rem]">
       <Table optionalWidth="w-[45%]" headers={headers} />

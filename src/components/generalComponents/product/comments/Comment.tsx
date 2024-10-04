4cd22199a29a39973d543877f3e68c493fe8948f
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-10-04 14:48:17 +0330

    create Comment component

--- src/components/generalComponents/product/comments/Comment.tsx
+++ src/components/generalComponents/product/comments/Comment.tsx
@@ -1,6 +1,50 @@
-import ProductCard from "../../../common/productCard/ProductCard";
+import { useParams } from "react-router-dom";
+import productService from "../../../../services/productService";
+import { useEffect, useState } from "react";
+
+interface Review {
+  comment: string;
+  createdAt: string;
+  name: string;
+  rating: number;
+  updateAt: string;
+  user: string;
+  _id: string;
+}
+
 const Comment = () => {
-  return <div className="bg-pink-400 w-full h-full">Comment</div>;
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const { id } = useParams();
+
+  const fetchProduct = async () => {
+    const res = await productService.getProduct(String(id));
+    setReviews(res.reviews);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+    reviews.map((rev) => console.log(rev));
+  }, [id]);
+
+  return (
+    <>
+      {reviews.map((review) => (
+        <div
+          className="bg-base-side w-full rounded-[0.8rem] p-6 flex flex-col gap-5"
+          key={review._id}
+        >
+          <div className="flex justify-between items-center text-text-secondary text-[1.4rem] font-normal">
+            <p>{review.name}</p>
+            <p>{review.createdAt}</p>
+          </div>
+          <div className="text-text-primary font-normal text-[1.4rem]">
+            {review.comment}
+          </div>
+          <div>{review.rating}</div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Comment;

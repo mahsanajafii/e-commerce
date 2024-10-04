70d29d4ff939b0c87c664e5f6e3fb17861fea33b
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-10-04 13:43:01 +0330

    CommentForm Compelete

--- src/components/generalComponents/product/commentForm/CommentForm.tsx
+++ src/components/generalComponents/product/commentForm/CommentForm.tsx
@@ -1,13 +1,33 @@
 import Button from "../../../common/button/Button";
-import { useEffect } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import productService from "../../../../services/productService";
+
 const CommentForm = () => {
   const { id } = useParams();
-  useEffect(() => {
-    console.log("from review : " + id);
-  }, []);
+  const [selectedScore, setSelectedScore] = useState("");
+  const [comment, setComment] = useState("");
+
+  const createRev = async () => {
+    return await productService.createReview(
+      Number(selectedScore),
+      comment,
+      String(id)
+    );
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (selectedScore && comment) {
+      createRev();
+      setSelectedScore("");
+      setComment("");
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-5">
+    <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
       <div className="flex flex-col justify-center items-start gap-5">
         <label
           htmlFor="score"
@@ -18,15 +38,17 @@ const CommentForm = () => {
         <select
           id="score"
           className="w-full h-14 rounded-xl px-5 outline-none border-slate-200 border-2 text-[1.6rem] font-normal"
+          value={selectedScore}
+          onChange={(e) => setSelectedScore(e.target.value)}
         >
           <option disabled value="">
             انتخاب امتیاز
           </option>
-          <option value="">1</option>
-          <option value="">2</option>
-          <option value="">3</option>
-          <option value="">4</option>
-          <option value="">5</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
         </select>
       </div>
       <div className="flex flex-col justify-center items-start gap-5">
@@ -40,6 +62,8 @@ const CommentForm = () => {
           id="comment-area"
           className="w-full h-52 rounded-xl p-5 outline-none border-slate-200 border-2 text-[1.6rem] font-normal"
           placeholder="نظر خود را وارد نمایید"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
         ></textarea>
       </div>
       <Button className="bg-primary-main text-text-button flex justify-center items-center px-[1.2rem] py-[0.8rem] rounded-[0.8rem] text-[1.4rem] font-normal w-[7.3rem] h-[4rem]">

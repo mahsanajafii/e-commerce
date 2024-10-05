0b068274c53502653f8a5d242b8418234b6ad3e1
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-10-06 00:12:26 +0330

    fix upload service

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -3,9 +3,7 @@ import axiosClient from "../api/axiosClient";
 const uploadService = {
     uploadImage: async (image: FormData) => {
         console.log(image)
-        const response = await axiosClient.post('/upload', {
-            image
-        }, {
+        const response = await axiosClient.post('/upload', image, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }

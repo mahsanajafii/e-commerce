988b60228a603ef4be6fe19fae516652126dfb0f
Marziye <pakdel.marzieh1380@gmail.com>
2024-10-21 14:41:47 +0330

    fix: :bug: notFound page

--- src/components/generalComponents/notFound/NotFound.tsx
+++ src/components/generalComponents/notFound/NotFound.tsx
@@ -1,8 +1,33 @@
-
+import { TbError404 } from "react-icons/tb";
+import { PiSmileySadLight } from "react-icons/pi";
+import MainLayout from "../../../layout/mainLayout/MainLayout";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/dashboard");
+      };
+
   return (
-    <div>NotFound</div>
+    <MainLayout>
+        <div className="flex flex-col w-full h-full items-center justify-center">
+            <TbError404 className="text-[20rem] text-primary-main" />
+            <p className="text-[1.6rem] mb-8 flex items-center gap-1">
+                متاسفانه صفحه مورد نظر شما پیدا نشد
+                <PiSmileySadLight className="text-[2rem]" />
+                    </p> 
+            <button
+                className="bg-primary-main rounded-full text-text-button text-[1.6rem] py-3 px-10 flex items-center gap-2"
+                onClick={handleClick}
+            >
+                بازگشت به خانه
+                <FaArrowLeftLong />
+            </button>
+        </div>
+    </MainLayout>
   )
 }
 

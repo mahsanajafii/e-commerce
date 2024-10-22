524c8147ff19b223bfd5a9ebe6ef4af58603c3c8
Marziye <pakdel.marzieh1380@gmail.com>
2024-10-22 15:33:57 +0330

    fix: :bug: admin menu dark mode styles

--- src/components/adminComponents/adminMenu/AdminMenu.tsx
+++ src/components/adminComponents/adminMenu/AdminMenu.tsx
@@ -14,20 +14,20 @@ const AdminMenu = () => {
     setIsChecked(!isChecked);
   };
   return (
-    <div className={`absolute left-2 top-2 rounded-xl flex z-10`}>
+    <div className={`absolute left-5 top-5 rounded-xl flex z-10`}>
       <LogoAdminMenu handelClick={handelClick} />
       <div
-        className={`absolute top-[2rem] w-[17rem] transition-opacity duration-500 ease-in-out bg-base-side border  h-[22.8rem] border-base-text-field-stroke py-6 px-3 gap-6 left-[2rem] rounded-xl ${
+        className={`absolute dark:bg-dark-base-side top-[2rem] w-[17rem] transition-opacity duration-500 ease-in-out bg-base-side border  h-[22.8rem] border-base-text-field-stroke dark:border-dark-base-text-field-stroke py-6 px-3 gap-6 left-[2rem] rounded-xl ${
           isChecked ? "block " : "hidden"
         } `}
       >
-        <div className="flex  flex-col gap-6 transition-width duration-700 ease-in-out justify-around items-center h-full text-text-primary text-2xl leading-8 font-normal">
+        <div className="flex flex-col gap-6 transition-width duration-700 ease-in-out justify-around items-center h-full text-text-primary text-2xl leading-8 font-normal">
           {AdminMenuList.map((item, index) => (
             <NavLink
               key={index}
               to={item.linkTo}
               onClick={() => handelClick()}
-              className={`transition-width duration-700 ease-in-out h-[3.7rem] gap-4 rounded-lg p-3 cursor-pointer hover:bg-[#DB277714] ${
+              className={`transition-width duration-700 ease-in-out h-[3.7rem] gap-4 rounded-lg p-3 cursor-pointer dark:text-dark-text-primary hover:bg-[#DB277714] hover:text-primary-main dark:hover:text-primary-main ${
                 isChecked ? " w-full" : " w-0"
               }`}
             >
--- src/components/adminComponents/adminMenu/logoAdminMenu/LogoAdminMenu.tsx
+++ src/components/adminComponents/adminMenu/logoAdminMenu/LogoAdminMenu.tsx
@@ -5,12 +5,12 @@ interface IHandelClick{
 }
 const LogoAdminMenu:React.FC<IHandelClick> = ({handelClick}) => {
   return (
-    <label >
-      <div className="w-16 h-14  bg-base-side cursor-pointer flex flex-col items-center justify-center">
+    <label>
+      <div className="w-16 h-14 bg-base-side dark:bg-dark-base-side cursor-pointer flex flex-col items-center justify-center rounded-[0.8rem] border border-base-text-field-stroke dark:border-dark-base-text-field-stroke">
         <input className="hidden peer" type="checkbox" onClick={handelClick} />
-        <div className="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.85rem]  peer-checked:rotate-[-45deg]"  />
-        <div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden" />
-        <div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.85rem] peer-checked:rotate-[45deg]" />
+        <div className="w-[50%] h-[2px] bg-black dark:bg-dark-icon-primary rounded-sm transition-all duration-300 origin-left translate-y-[0.85rem]  peer-checked:rotate-[-45deg]"  />
+        <div className="w-[50%] h-[2px] bg-black dark:bg-dark-icon-primary rounded-md transition-all duration-300 origin-center peer-checked:hidden" />
+        <div className="w-[50%] h-[2px] bg-black dark:bg-dark-icon-primary rounded-md transition-all duration-300 origin-left -translate-y-[0.85rem] peer-checked:rotate-[45deg]" />
       </div>
     </label>
   );

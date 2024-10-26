df29c50b470c9fb5b7f60ae62139acecbf8c34ee
Marziye <pakdel.marzieh1380@gmail.com>
2024-10-26 16:11:20 +0330

    fix: :bug:

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,7 +1,15 @@
 import { Outlet } from "react-router-dom";
 import MainLayout from "../../layout/mainLayout/MainLayout";
+import useThemeStore from "../../stores/themeStore";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
   return (
     <MainLayout>
       <Outlet></Outlet>

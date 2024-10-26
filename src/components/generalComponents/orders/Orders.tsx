5acaca545ae9192ff0a9b43efc08d89ff19adaae
Marziye <pakdel.marzieh1380@gmail.com>
2024-10-26 23:46:26 +0330

    fix: :bug:

--- src/components/generalComponents/orders/Orders.tsx
+++ src/components/generalComponents/orders/Orders.tsx
@@ -49,10 +49,6 @@ const Orderscopy: React.FC = () => {
             ارسال: order.isDelivered ? "ارسال شده" : "ارسال نشده",
             // عملیات: "جزئیات",
             orderId: order._id,
-            navigate:
-              order.isPaid && order.isDelivered
-                ? `/detail/deliverd/${order._id}`
-                : `/checkout/${order._id}`,
           }))
         )
         .flat();
@@ -73,7 +69,6 @@ const Orderscopy: React.FC = () => {
             ارسال: order.isDelivered ? "ارسال شده" : "ارسال نشده",
             // عملیات: "جزئیات",
             orderId: order._id,
-            navigate: `/detail/${order._id}`,
           }))
         )
         .flat();
@@ -81,25 +76,25 @@ const Orderscopy: React.FC = () => {
       setOrdersTitle(res);
     }
   }
-  const handelClick = (id) => {
+  const handleClick = (id) => {
     setIsActive((prev) => (prev === id ? null : id));
   };
   return (
-    <div className="min-h-screen pt-24 px-8  flex  transition-height duration-700 ease-in-out flex-col gap-4">
+    <div className="min-h-screen pt-24 px-8 flex transition-height duration-700 ease-in-out flex-col gap-4">
       <h1 className="text-[3rem] text-primary-main">{` لیست سفارشها : ${ordersTitle.length} مورد`}</h1>
       <br />
       {ordersTitle.map((order, i) => (
         <div key={i} className="flex flex-col justify-between">
           <button
-            onClick={() => handelClick(order._id)}
-            className="w-full flex flex-row h-32 bg-white items-center rounded-3xl p-8 text-[1.6rem]"
+            onClick={() => handleClick(order._id)}
+            className="w-full flex flex-row h-32 bg-white dark:bg-dark-base-side border hover:shadow-md dark:hover:shadow-gray-400 dark:hover:shadow-sm border-base-text-field-stroke dark:border-dark-base-text-field-stroke items-center rounded-3xl p-8 text-[1.6rem] dark:text-dark-text-primary"
           >
             <IoIosArrowDropdownCircle className={`text-[2.4rem] ml-10 text-primary-main transition-all duration-400 ease-in-out ${isActive === order._id ? "rotate-90" : "rotate-0"}`} />
-            <span className=" w-[35%] text-right">شماره سفارش : {order._id}</span>
-            <span className=" w-[20%] text-right ">
+            <span className="w-[35%] text-right">شماره سفارش : {order._id}</span>
+            <span className="w-[20%] text-right">
               مبلغ کل : {`${order.totalPrice.toLocaleString("fa-IR")} تومان`}
             </span>
-            <span className=" w-[20%] text-green-700">
+            <span className="w-[20%] text-primary-main">
               وضعیت :{" "}
               {order.isDelivered
                 ? "ارسال شده"
@@ -107,23 +102,28 @@ const Orderscopy: React.FC = () => {
             </span>
             <span className="w-[20%]">
               تاریخ ثبت سفارش :{" "}
-              {typeof order.createdAt === "string" ||
-              typeof order.createdAt === "number"
+              {typeof order.createdAt === "string" || typeof order.createdAt === "number"
                 ? new Date(order.createdAt).toLocaleDateString("fa-IR")
                 : "تاریخ مشخص نیست"}
             </span>
             <NavLink
-              to={`/detail/${order._id}`}
+              to={
+                isAdmin()
+                  ? order.isPaid && order.isDelivered
+                    ? `/detail/deliverd/${order._id}`
+                    : `/checkout/${order._id}`
+                  : `/detail/${order._id}`
+              }
               className="bg-primary-main px-[1.2rem] py-[0.8rem] rounded-[0.8rem] text-[1.4rem] text-text-button font-normal"
             >
               جزئیات
             </NavLink>
           </button>
-
+  
           <div
             className={`transition-all duration-700 ease-in-out mt-10 ${
               isActive === order._id ? "block" : "hidden"
-            } bg-primary-lighter`}
+            } bg-primary-lighter dark:bg-[#4a0d29] rounded-3xl`}
           >
             <Table
               optionalWidth="w-full"
@@ -134,7 +134,7 @@ const Orderscopy: React.FC = () => {
         </div>
       ))}
     </div>
-  );
+  );  
 };
 
 export default Orderscopy;
